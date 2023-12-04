334d27b7895809da52d2920a45af328159da764f
timadigwe <timothyadigwe2019@gmail.com>
2023-12-04 16:05:12 +0100

    view functions implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,122 +1,293 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 //! # A Concordium V1 smart contract
+
 use concordium_std::*;
 use core::fmt::Debug;
+use concordium_std::Amount;
+use rand::prelude::*;
 
 
 
-
-// Struct to represent a product listing.
-#[derive(Serialize, SchemaType)]
-pub struct Listing {
-    product_name: String,
-    price: Amount,
-    seller: AccountAddress,
-}
-
-// Struct to represent an order.
+/// Enum representing the possible states of a product
 #[derive(Debug, Serialize, SchemaType, PartialEq, Eq, Clone)]
-pub struct Order {
-    product_name: String,
-    price: Amount,
-    seller: AccountAddress,
-    buyer: AccountAddress
-}
-
-#[derive(Debug, Serialize, SchemaType, PartialEq, Eq, Clone)]
-pub struct Escrow {
-    order_index: u64,
-    funds: Amount,
-}
-
-// Enum to represent order status.
-#[derive(Debug, PartialEq, Eq, Serialize, SchemaType)]
-pub enum OrderStatus {
-    Placed,
+pub enum ProductState {
+    Listed,
+    Released,
     Shipped,
-    Completed,
     Cancelled,
 }
 
-#[derive(Serialize, SchemaType)]
-pub struct InitParameter;
-
-/// 'add_listing' function errors
-#[derive(Debug, PartialEq, Eq, Clone, Reject, Serialize, SchemaType)]
-pub enum AddListingError {
-    /// Raised when an account other than the seller tries to add a listing
-    NotSeller,
+// Struct to represent a product listing.
+#[derive(Serialize, SchemaType,  Clone)]
+pub struct ProductListing {
+    pub id: u128,  // Unique identifier for the listing
+    pub farmer: AccountAddress,
+    pub product: String,
+    pub price: Amount,
+    pub state: ProductState
 }
 
 
-/// 'place_order' function errors
-#[derive(Debug, PartialEq, Eq, Clone, Reject, Serialize, SchemaType)]
-pub enum PlaceOrderError {
-    /// Raised when placing an order for a non-existent listing
-    ListingNotFound,
-    /// Raised when a buyer tries to place an order for their own listing
-    CannotBuyOwnListing,
-    /// Raised when placing an order with insufficient funds
-    InsufficientFunds,
+// Struct to represent an order.
+#[derive( Serialize, SchemaType, Eq, PartialEq, PartialOrd, Clone)]
+pub struct Order {
+    pub buyer: AccountAddress,
+    pub product: String,
+    pub price: Amount,
 }
 
-/// 'view_listings' function errors
-#[derive(Debug, PartialEq, Eq, Clone, Reject, Serialize, SchemaType)]
-pub enum ViewOrdersError {
-    /// An error occurred while accessing the orders.
-
+#[derive(Serialize, SchemaType, Eq, PartialEq, PartialOrd, Clone)]
+pub struct Escrow {
+    pub funds: Amount,
+    pub product: String,
+    pub buyer: AccountAddress,
 }
 
-/// Your smart contract errors.
-#[derive(Debug, PartialEq, Eq, Reject, Serialize, SchemaType)]
-pub enum Error {
-    /// Failed parsing the parameter.
-    #[from(ParseError)]
-    ParseParams,
-    /// Your error
+
+/// Smart contract state
+#[derive( Serialize, SchemaType,  Clone)]
+pub struct State {
+    pub product_listings: Vec<ProductListing>,
+    pub orders: Vec<Order>,
+    pub escrows: Vec<Escrow>,
+}
+
+/// Error types
+#[derive(Debug, PartialEq, Eq, Clone, Reject, Serialize, SchemaType)]
+pub enum MarketplaceError {
     ProductNotFound,
     OrderNotFound,
-    InvalidOrderStatus,
+    EscrowNotFound,
+    InvalidCaller,
+    InvalidProductState,
     InsufficientFunds,
+    InvalidPrice,
+    RandomGenerationError,
+    #[from(ParseError)]
+    ParseParams,
+    #[from(TransferError)]
+    TransferError,
 }
 
-/// Initialization function for the marketplace smart contract
-#[init(contract = "gonana_concordium_smart_contract")]
-fn init(_ctx: &InitContext, _state_builder: &mut StateBuilder) -> InitResult<State> {
-    // Your code
+#[derive(Serialize, SchemaType)]
+pub struct ListProductParameter{
+    pub farmer: AccountAddress,
+    pub product: String,
+    pub price: Amount,
+}
 
-    Ok(State {
-        products: Vec::new(),
+#[derive(Serialize, SchemaType)]
+pub struct CancelProductParameter{
+    pub product: String,
+}
+
+#[derive(Serialize, SchemaType)]
+pub struct PlaceOrderParameter {
+    pub product_id: u128,
+    pub buyer:AccountAddress,
+    pub price: Amount
+}
+
+// Init function to initialize the marketplace state
+#[init(contract = "gonana_marketplace")]
+fn init(_ctx: &InitContext, _state_builder: &mut StateBuilder) -> InitResult<State>{
+    Ok(State { 
+         product_listings: Vec::new(),
         orders: Vec::new(),
-    })
+        escrows: Vec::new(),
+     })
 }
 
-pub type MyInputType = bool;
 
-/// Receive function. The input parameter is the boolean variable `throw_error`.
-///  If `throw_error == true`, the receive function will throw a custom error.
-///  If `throw_error == false`, the receive function executes successfully.
-#[receive(
-    contract = "gonana_concordium_smart_contract",
-    name = "receive",
-    parameter = "MyInputType",
-    error = "Error",
-    mutable
-)]
-fn receive(ctx: &ReceiveContext, _host: &mut Host<State>) -> Result<(), Error> {
-    // Your code
+/// Function to list a product in the marketplace
+#[receive(contract = "gonana_marketplace", name = "list_product", parameter = "ListProductParameter", mutable )]
+fn list_product(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), MarketplaceError>{
+    let parameter: ListProductParameter= ctx.parameter_cursor().get()?;
+   
+ // Generate a unique ID for the product listing using the rand crate
+ let id: u128 = random();
 
-    let throw_error = ctx.parameter_cursor().get()?; // Returns Error::ParseError on failure
-    if throw_error {
-        Err(Error::YourError)
-    } else {
-        Ok(())
+    //check if the price is 0
+    if parameter.price <= Amount::zero() {
+        return Err(MarketplaceError::InvalidPrice);
+    }
+
+    // Check if the product name is empty
+    if parameter.product.is_empty() {
+        return Err(MarketplaceError::ParseParams);
+    }
+
+    let listing = ProductListing {
+        id,
+        farmer: parameter.farmer,
+        product: parameter.product,
+        price: parameter.price,
+        state: ProductState::Listed
+    };
+        host.state_mut().product_listings.push(listing);
+        Ok(()) 
+}
+
+
+/// Function to cancel or unlist a product
+#[receive(contract = "gonana_marketplace", name = "cancel_product", parameter = "CancelProductParameter", mutable )]
+fn cancel_product(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), MarketplaceError>{
+    let parameter: CancelProductParameter= ctx.parameter_cursor().get()?;
+    let product = parameter.product;
+
+
+    let mut listing = host.state_mut().product_listings.iter_mut().find(|l| l.product == product)
+    .ok_or(MarketplaceError::ProductNotFound)?;
+
+     // Check if the product is in a cancellable state
+     match listing.state {
+        ProductState::Listed | ProductState::Released => {
+            listing.state = ProductState::Cancelled;
+            Ok(())
+        }
+        _ => Err(MarketplaceError::InvalidProductState),
     }
 }
 
-/// View function that returns the content of the state.
-#[receive(contract = "gonana_concordium_smart_contract", name = "view", return_value = "State")]
-fn view<'b>(_ctx: &ReceiveContext, host: &'b Host<State>) -> ReceiveResult<&'b State> {
-    Ok(host.state())
+
+/// Function to place an order and create an escrow
+#[receive(contract = "gonana_marketplace", name="place_order", parameter = "PlaceOrderParameter", mutable, payable)]
+fn place_order(ctx: &ReceiveContext, host: &mut Host<State>, _amount: Amount) -> Result<(), MarketplaceError>{
+    let parameter : PlaceOrderParameter = ctx.parameter_cursor().get()?;
+
+     // Check if the product ID is valid
+    // if host.state_mut().product_listings.iter().find(|listing| listing.id == parameter.product_id)
+    // .is_none() {
+    //     return Err(MarketplaceError::ProductNotFound);
+    // }
+
+    // Find the product by ID
+    if let Some(product) = host.state_mut().product_listings.clone().iter_mut().find(|p| p.id == parameter.product_id) {
+        // Check if the product is in a valid state for placing an order
+        if product.state != ProductState::Listed {
+            return Err(MarketplaceError::InvalidProductState);
+        }
+        
+         // Create an order
+         let order = Order {
+            buyer: parameter.buyer,
+            product: product.product.clone(),
+            price: parameter.price,
+        };
+
+         // Deduct the order amount from the buyer's account
+         host.invoke_transfer(&product.farmer, parameter.price)?;
+
+          // Add the order to the orders list
+        host.state_mut().orders.push(order);
+
+          // Update the product state to Released
+          product.state = ProductState::Released;
+
+           // Create an escrow
+        let escrow = Escrow {
+            funds: parameter.price,
+            product: product.product.clone(),
+            buyer: parameter.buyer,
+        };
+
+        // Add the escrow to the escrows list
+        host.state_mut().escrows.push(escrow);
+
+        Ok(())
+    } else {
+        // Product not found
+        Err(MarketplaceError::ProductNotFound)
+    }
+
+ 
+}
+
+
+
+/// Function to release a product in the marketplace
+#[receive(contract = "gonana_marketplace", name = "release_product", parameter = "CancelProductParameter", mutable)]
+fn release_product(ctx:&ReceiveContext, host: &mut Host<State>) -> Result<(), MarketplaceError>  {
+    let parameter: CancelProductParameter = ctx.parameter_cursor().get()?;
+
+    // Check if the product name is not empty
+    if parameter.product.is_empty() {
+        return Err(MarketplaceError::ParseParams);
+    }
+
+     // Find the product by name
+     if let Some(product) = host.state_mut().product_listings.iter_mut().find(|p| p.product == parameter.product) {
+        // Check if the product is in a valid state for releasing
+        if product.state != ProductState::Released {
+            return Err(MarketplaceError::InvalidProductState);
+        }
+
+        // Update the product state to Shipped
+        product.state = ProductState::Shipped;
+
+        Ok(())
+    } else {
+        // Product not found
+        Err(MarketplaceError::ProductNotFound)
+    }
+    
+}
+
+
+
+/// Function to confirm an escrow in the marketplace
+#[receive( contract = "gonana_marketplace", name = "confirm_escrow", parameter = "CancelProductParameter", mutable )]
+fn confirm_escrow(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), MarketplaceError>{
+    let parameter: CancelProductParameter = ctx.parameter_cursor().get()?;
+
+    // Check if the product name is not empty
+    if parameter.product.is_empty() {
+        return Err(MarketplaceError::ParseParams);
+    }
+
+      // Find the product by name
+    if let Some(product) = host.state_mut().product_listings.clone().iter_mut().find(|p| p.product == parameter.product) {
+        // Check if the product is in a valid state for confirming the escrow
+        if product.state != ProductState::Cancelled {
+            return Err(MarketplaceError::InvalidProductState);
+        }
+
+        // Find the corresponding escrow
+        if let Some(escrow_index) = host.state_mut().escrows.iter().position(|e| e.product == parameter.product) {
+            let escrow = host.state_mut().escrows.remove(escrow_index);
+
+            // Transfer the funds from the escrow to the farmer's account
+            host.invoke_transfer(&product.farmer, escrow.funds)?;
+
+            Ok(())
+        } else {
+            // Escrow not found
+            Err(MarketplaceError::EscrowNotFound)
+        }
+    } else {
+        // Product not found
+        Err(MarketplaceError::ProductNotFound)
+    }
+   
+}
+
+
+
+// View function to get all product listings
+#[receive(contract = "gonana_marketplace", name = "view_product_listings", return_value = "Vec<ProductListing>")]
+fn view_product_listings(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<Vec<ProductListing>> {
+    Ok(host.state().product_listings.clone())
+}
+
+// View function to get all orders
+#[receive(contract = "gonana_marketplace", name = "view_orders", return_value = "Vec<Order>")]
+fn view_orders(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<Vec<Order>> {
+    Ok(host.state().orders.clone())
+}
+
+// View function to get all escrows
+#[receive(contract = "gonana_marketplace", name = "view_escrows", return_value = "Vec<Escrow>")]
+fn view_escrows(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<Vec<Escrow>> {
+    Ok(host.state().escrows.clone())
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -11,73 +11,69 @@ const ACC_INITIAL_BALANCE: Amount = Amount::from_ccd(10_000);
 /// A [`Signer`] with one set of keys, used for signing transactions.
 const SIGNER: Signer = Signer::with_one_key();
 
-/// Test that invoking the `receive` endpoint with the `false` parameter
-/// succeeds in updating the contract.
-#[test]
-fn test_throw_no_error() {
-    let (mut chain, init) = initialize();
 
-    // Update the contract via the `receive` entrypoint with the parameter `false`.
-    chain
-        .contract_update(SIGNER, ALICE, ALICE_ADDR, Energy::from(10_000), UpdateContractPayload {
-            address:      init.contract_address,
-            amount:       Amount::zero(),
-            receive_name: OwnedReceiveName::new_unchecked("gonana_concordium_smart_contract.receive".to_string()),
-            message:      OwnedParameter::from_serial(&false)
-                .expect("Parameter within size bounds"),
-        })
-        .expect("Update succeeds with `false` as input.");
-}
-
-/// Test that invoking the `receive` endpoint with the `true` parameter
-/// results in the `YourError` being thrown.
-#[test]
-fn test_throw_error() {
-    let (mut chain, init) = initialize();
-
-    // Update the contract via the `receive` entrypoint with the parameter `true`.
-    let update = chain
-        .contract_update(SIGNER, ALICE, ALICE_ADDR, Energy::from(10_000), UpdateContractPayload {
-            address:      init.contract_address,
-            amount:       Amount::zero(),
-            receive_name: OwnedReceiveName::new_unchecked("gonana_concordium_smart_contract.receive".to_string()),
-            message:      OwnedParameter::from_serial(&true).expect("Parameter within size bounds"),
-        })
-        .expect_err("Update fails with `true` as input.");
-
-    // Check that the contract returned `YourError`.
-    let error: Error = update.parse_return_value().expect("Deserialize `Error`");
-    assert_eq!(error, Error::YourError);
-}
-
-/// Helper method for initializing the contract.
-///
-/// Does the following:
-///  - Creates the [`Chain`]
-///  - Creates one account, `Alice` with `10_000` CCD as the initial balance.
-///  - Initializes the contract.
-///  - Returns the [`Chain`] and the [`ContractInitSuccess`]
-fn initialize() -> (Chain, ContractInitSuccess) {
-    // Initialize the test chain.
+fn setup_chain_and_contract() -> (Chain, ContractInitSuccess) {
     let mut chain = Chain::new();
 
-    // Create the test account.
     chain.create_account(Account::new(ALICE, ACC_INITIAL_BALANCE));
 
-    // Load the module.
-    let module = module_load_v1("./concordium-out/module.wasm.v1").expect("Module exists at path");
-    // Deploy the module.
-    let deployment = chain.module_deploy_v1(SIGNER, ALICE, module).expect("Deploy valid module");
+    let module = module_load_v1("gonana_marketplace.wasm.v1").expect("Module is valid and exists");
+    let deployment = chain.module_deploy_v1(SIGNER,ALICE , module).expect("Deploying valid module should succeed");
 
-    // Initialize the contract.
-    let init = chain
-        .contract_init(SIGNER, ALICE, Energy::from(10_000), InitContractPayload {
-            amount:    Amount::zero(),
-            mod_ref:   deployment.module_reference,
-            init_name: OwnedContractName::new_unchecked("init_gonana_concordium_smart_contract".to_string()),
-            param:     OwnedParameter::empty(),
-        })
-        .expect("Initializing contract");
+    let initialization = chain
+    .contract_init(
+        SIGNER,
+        ALICE,
+        Energy::from(10000),
+        InitContractPayload {
+            mod_ref: deployment.module_reference,
+            init_name: OwnedContractName::new_unchecked("init_gonana_marketplace".to_string()),
+            param: OwnedParameter::empty(),
+            amount: Amount::zero(),
+        }
+    )
+    .expect("Initialization should always succeed"); 
 
-    (chain, init)
+    (chain, initialization)
+}
+/// Test listing a product in the marketplace.
+#[test]
+fn test_list_product() {
+    let (mut chain, init) = setup_chain_and_contract();
+
+    // List a product in the marketplace.
+    chain
+        .contract_update(
+            SIGNER,
+            ALICE,
+            ALICE_ADDR,
+            Energy::from(10_000),
+            UpdateContractPayload {
+                address:      init.contract_address,
+                amount:       Amount::zero(),
+                receive_name: OwnedReceiveName::new_unchecked("gonana_marketplace.list_product".to_string()),
+                message:      OwnedParameter::from_serial(&ListProductParameter {
+                    farmer: ALICE,
+                    product: "Apples".to_string(),
+                    price: Amount::from_ccd(100),
+                })
+                .expect("Parameter within size bounds"),
+            },
+        )
+        .expect("List product succeeds.");
+
+    // Verify that the product is listed.
+    let product_listings: Vec<ProductListing> =
+        chain.contract_update(SIGNER, ALICE,  Address::Account(ALICE),  Energy::from(10000), UpdateContractPayload {
+            amount: Amount::zero(),
+            address: init.contract_address,
+            receive_name: OwnedReceiveName::new_unchecked("ccdpiggybank.insert".to_string()),
+            message: OwnedParameter::empty(),
+        },)
+            .expect("View product listings succeeds.")
+            .parse_return_value()
+            .expect("Deserialize product listings");
+
+    assert_eq!(product_listings.len(), 1);
+    assert_eq!(product_listings[0].product, "Apples");
 }
