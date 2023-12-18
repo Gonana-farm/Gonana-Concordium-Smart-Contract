a0e64125fa45da5e491a193d60fd020a46186056
timadigwe <timothyadigwe2019@gmail.com>
2023-12-18 14:07:59 +0100

    schema

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,11 @@ use concordium_std::Amount;
 #[derive(Debug, Serialize, SchemaType, PartialEq, Eq, Clone)]
 pub enum ProductState {
     Listed,
-    Released,
+    Escrowed,
     Confirmed,
     Cancelled,
 }
+
 
 // Struct to represent a product listing.
 #[derive(Serialize, Clone, SchemaType, PartialEq, Eq, Debug)]
@@ -34,31 +35,8 @@ pub struct Order {
     pub price: Amount,
 }
 
-#[derive(Serialize, SchemaType, Eq, PartialEq, PartialOrd, Clone)]
-pub struct Escrow {
-    pub funds: Amount,
-    pub product: String,
-    pub buyer: AccountAddress,
-}
 
 
-/// Smart contract state
-#[derive(Serial, DeserialWithState)]
-#[concordium(state_parameter = "S")]
-pub struct State<S = StateApi>  {
-    pub product_listings: StateMap<String,ProductListing, S>,
-    pub orders: StateMap<String,Order,S>,
-    pub escrows: StateMap<String,Escrow,S>,
-}
-
-impl<S: HasStateApi> State<S> {
-    pub fn get_product_by_name(&self, product_name: &String) -> Result<ProductListing, MarketplaceError> {
-        self.product_listings
-            .get(product_name)
-            .ok_or(MarketplaceError::ProductNotFound)
-            .map(|product| product.clone())
-    }
-}
 
 /// Error types
 #[derive(Debug, PartialEq, Eq, Clone, Reject, Serialize, SchemaType)]
@@ -66,7 +44,6 @@ pub enum MarketplaceError {
     ProductNotFound,
     OrderNotFound,
     OrderAlreadyExists,
-    EscrowNotFound,
     InvalidProductState,
     InsufficientFunds,
     InvalidPrice,
@@ -92,16 +69,25 @@ pub struct CancelProductParameter{
 pub struct PlaceOrderParameter {
     pub product_name: String,
     pub buyer:AccountAddress,
-    pub price: Amount
+    pub seller: AccountAddress,
 }
+
+
+/// Smart contract state
+#[derive(Serial, DeserialWithState)]
+#[concordium(state_parameter = "S")]
+pub struct State<S = StateApi>  {
+    pub product_listings: StateMap<String,ProductListing, S>,
+    pub orders: StateMap<String,Order,S>,
+}
+
 
 // Init function to initialize the marketplace state
 #[init(contract = "gonana_marketplace")]
 fn init(_ctx: &InitContext, state_builder: &mut StateBuilder) -> InitResult<State>{
     Ok(State { 
-         product_listings: state_builder.new_map(),
-        orders: state_builder.new_map(),
-        escrows: state_builder.new_map(),
+            product_listings: state_builder.new_map(),
+            orders: state_builder.new_map(),
      })
 }
 
@@ -145,7 +131,7 @@ fn cancel_product(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), Ma
     if let Some(mut listing) = host.state_mut().product_listings.get_mut(&product_name) {
         // Check if the product is in a cancellable state
         match listing.state {
-            ProductState::Listed | ProductState::Released => {
+            ProductState::Listed | ProductState::Escrowed => {
                 listing.state = ProductState::Cancelled;
                 Ok(())
             }
@@ -161,77 +147,73 @@ fn cancel_product(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), Ma
 fn place_order(ctx: &ReceiveContext, host: &mut Host<State>, amount: Amount) -> Result<(), MarketplaceError> {
     let parameter: PlaceOrderParameter = ctx.parameter_cursor().get()?;
 
-      // Find the product by name using the new method
-    let mut product = host.state().get_product_by_name(&parameter.product_name)?;
-       
+    let state_mut = host.state_mut();
 
-        // Ensure that the product is in a valid state for placing an order
+    // Find the product by name
+    let mut product = 
+        state_mut
+        .product_listings
+        .get_mut(&parameter.product_name)
+        .ok_or(MarketplaceError::ProductNotFound)?;
+
+    // Ensure that the product is in a valid state for placing an order
     ensure!(product.state == ProductState::Listed, MarketplaceError::InvalidProductState);
-    
-      // Ensure that the order price is greater than zero
-      ensure!(parameter.price > Amount::zero(), MarketplaceError::InvalidPrice);
-
-      // Ensure that the buyer has enough funds
-     // Ensure that the buyer has enough funds
-     ensure!(parameter.price <= amount, MarketplaceError::InsufficientFunds);
 
 
-     // Create an order
-     let order = Order {
-        buyer: parameter.buyer,
+    // Create an order
+    let order = Order {
+        buyer:   parameter.buyer,
         product: product.product.clone(),
-        price: parameter.price,
+        price:   amount,
     };
 
-   // Deduct the order amount from the buyer's account
-   host.invoke_transfer(&product.farmer, parameter.price)?;
+    // Insert the order and update the product state to Escrowed 
+    ensure!(state_mut.orders.insert(parameter.product_name.clone(), order).is_none(), MarketplaceError::OrderAlreadyExists);
 
-   // Insert the order and update the product state to Released in one statement
-   if host.state_mut().orders.insert(parameter.product_name.clone(), order).is_none() {
     // If the insert is successful, update the product state
-    product.state = ProductState::Released;
+     // Update the product state to Escrowed
+     product.state = ProductState::Escrowed;
     Ok(())
-} else {
-    // Handle the case when the order already exists
-    Err(MarketplaceError::OrderAlreadyExists)
-}
 
 }
 
 
+//function to confirm an escrow 
+#[receive(contract = "gonana_marketplace", name = "confirm_order", parameter = "PlaceOrderParameter", mutable)]
+fn confirm_order(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), MarketplaceError> {
+     let param:PlaceOrderParameter = ctx.parameter_cursor().get()?;
+     let product_name = param.product_name.clone();
 
+     let state_mut = host.state_mut();
 
-/// Function to confirm an escrow in the marketplace
-#[receive(contract = "gonana_marketplace", name = "confirm_escrow", parameter = "CancelProductParameter", mutable)]
-fn confirm_escrow(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), MarketplaceError> {
-    let parameter: CancelProductParameter = ctx.parameter_cursor().get()?;
-    let product_name = parameter.product_name.clone();
+      // Find the product by name
+    let mut product = 
+    state_mut
+        .product_listings
+        .get_mut(&product_name)
+        .ok_or(MarketplaceError::ProductNotFound)?;
 
-     // Find the product by name
-     let mut product = {
-        let state = host.state_mut();
-        state.product_listings.remove_and_get(&product_name).ok_or(MarketplaceError::ProductNotFound)?
-    };
-
-     // Ensure that the product is in a valid state for confirming the escrow
-     ensure!(product.state == ProductState::Released, MarketplaceError::InvalidProductState);
-
-   // Remove the escrow and get its value
-   if let Some(escrow) = host.state_mut().escrows.remove_and_get(&product_name) {
-    // Transfer funds
-    host.invoke_transfer(&product.farmer, escrow.funds)?;
-
-    // It's important to call `Deletable::delete` on the value
-    escrow.delete();
-    // If the removal is successful, update the product state
+    // Ensure that the product is in a valid state for confirming the escrow
+    ensure!(product.state == ProductState::Escrowed, MarketplaceError::InvalidProductState);
+    
     product.state = ProductState::Confirmed;
+    drop(product);
 
-    Ok(())
-} else {
-    // Escrow not found
-    Err(MarketplaceError::EscrowNotFound)
+     // Get the order by product_name
+     if let Some(order) = host.state_mut().orders.remove_and_get(&param.product_name) {
+        // Transfer funds
+        host.invoke_transfer(&param.seller, order.price)?;
+
+        // It's important to call `Deletable::delete` on the value
+        order.delete();
+
+        Ok(())
+    } else {
+        // Escrow not found
+        Err(MarketplaceError::OrderNotFound)
+    }
 }
-}
+
 
 
 // // View function to get all product listings
@@ -250,10 +232,3 @@ fn view_orders(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<Vec<O
     Ok(orders)
 }
 
-// // View function to get all escrows
-#[receive(contract = "gonana_marketplace", name = "view_escrows", return_value = "Vec<Escrow>")]
-fn view_escrows(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<Vec<Escrow>> {
-    let state = host.state();
-    let escrows: Vec<Escrow> = state.escrows.iter().map(|(_, escrow)| escrow.clone()).collect();
-    Ok(escrows)
-}
--- tests/tests.rs
+++ tests/tests.rs
@@ -3,7 +3,10 @@ use gonana_concordium_smart_contract::*;
 
 /// A test account.
 const ALICE: AccountAddress = AccountAddress([0u8; 32]);
+const BOB: AccountAddress = AccountAddress([1u8; 32]);
 const ALICE_ADDR: Address = Address::Account(ALICE);
+const BOB_ADDR: Address = Address::Account(BOB);
+
 
 /// The initial balance of the ALICE test account.
 const ACC_INITIAL_BALANCE: Amount = Amount::from_ccd(10_000);
@@ -16,6 +19,7 @@ fn setup_chain_and_contract() -> (Chain, ContractInitSuccess) {
     let mut chain = Chain::new();
 
     chain.create_account(Account::new(ALICE, ACC_INITIAL_BALANCE));
+    chain.create_account(Account::new(BOB, ACC_INITIAL_BALANCE));
 
     let module = module_load_v1("gonana_marketplace.wasm.v1").expect("Module is valid and exists");
     let deployment = chain.module_deploy_v1(SIGNER,ALICE , module).expect("Deploying valid module should succeed");
@@ -32,17 +36,18 @@ fn setup_chain_and_contract() -> (Chain, ContractInitSuccess) {
             amount: Amount::zero(),
         }
     )
-    .expect("Initialization should always succeed"); 
+    .expect("Initialization should always succeed");
 
     (chain, initialization)
 }
-// Test listing a product in the marketplace.
-// Test placing an order in the marketplace.
+
+
+// Test confirming an escrow in the marketplace.
 #[test]
-fn test_place_order() {
+fn test_confirm_escrow() {
     let (mut chain, init) = setup_chain_and_contract();
 
-    // List a product in the marketplace.
+    //List a product in the marketplace.
     chain
         .contract_update(
             SIGNER,
@@ -50,57 +55,111 @@ fn test_place_order() {
             ALICE_ADDR,
             Energy::from(10_000),
             UpdateContractPayload {
-                address:      init.contract_address,
-                amount:       Amount::zero(),
+                address: init.contract_address,
+                amount: Amount::zero(),
                 receive_name: OwnedReceiveName::new_unchecked("gonana_marketplace.list_product".to_string()),
-                message:      OwnedParameter::from_serial(&ListProductParameter {
+                message: OwnedParameter::from_serial(&ListProductParameter {
                     farmer: ALICE,
-                    product: "Apples".to_string(),
-                    price: Amount::from_ccd(100),
+                    product: "Oranges".to_string(),
+                    price: Amount::from_ccd(200),
                 })
                 .expect("Parameter within size bounds"),
             },
         )
         .expect("List product succeeds.");
 
-    // View the product listings
+    //View the product listings
     let product_listings: Vec<ProductListing> =
-        chain.contract_update(SIGNER, ALICE,  Address::Account(ALICE),  Energy::from(10000), UpdateContractPayload {
-            amount: Amount::zero(),
-            address: init.contract_address,
-            receive_name: OwnedReceiveName::new_unchecked("gonana_marketplace.view_product_listings".to_string()),
-            message: OwnedParameter::empty(),
-        },)
+        chain
+            .contract_update(
+                SIGNER,
+                ALICE,
+                Address::Account(ALICE),
+                Energy::from(10000),
+                UpdateContractPayload {
+                    amount: Amount::zero(),
+                    address: init.contract_address,
+                    receive_name: OwnedReceiveName::new_unchecked("gonana_marketplace.view_product_listings".to_string()),
+                    message: OwnedParameter::empty(),
+                },
+            )
             .expect("View product listings succeeds.")
             .parse_return_value()
             .expect("Deserialize product listings");
 
     assert_eq!(product_listings.len(), 1);
-    assert_eq!(product_listings[0].product, "Apples");
+    assert_eq!(product_listings[0].product, "Oranges");
+
+    let alice_balance_before = chain.account_balance(ALICE).unwrap();
+    let bob_balance_before = chain.account_balance(BOB).unwrap();
+    let purchase_amount = Amount::from_ccd(200);
 
     // Place an order in the marketplace.
-    chain
+   let update = chain
         .contract_update(
             SIGNER,
-            ALICE,
-            ALICE_ADDR,
+            BOB,
+            BOB_ADDR,
             Energy::from(10_000),
             UpdateContractPayload {
-                address:      init.contract_address,
-                amount:       Amount::from_ccd(200),
+                address: init.contract_address,
+                amount: purchase_amount,
                 receive_name: OwnedReceiveName::new_unchecked("gonana_marketplace.place_order".to_string()),
-                message:      OwnedParameter::from_serial(&PlaceOrderParameter {
-                    product_name: "Apples".to_string(),
-                    buyer: ALICE,
-                    price: Amount::from_ccd(100),
+                message: OwnedParameter::from_serial(&PlaceOrderParameter {
+                    product_name: "Oranges".to_string(),
+                    buyer: BOB,
+                    seller: ALICE,
                 })
                 .expect("Parameter within size bounds"),
             },
-        )
-        .expect_err("Place order succeeds.");
+        );
+        
+        assert!(update.is_ok());
+    assert_eq!(chain.account_balance(BOB).unwrap().total, (bob_balance_before.total - purchase_amount - update.as_ref().unwrap().transaction_fee));
+    assert_eq!(chain.contract_balance(init.contract_address), Some(purchase_amount)); 
+
+    // Confirm the escrow in the marketplace.
+ let update = chain
+ .contract_update(
+     SIGNER,
+     BOB,
+     BOB_ADDR,
+     Energy::from(10_000),
+     UpdateContractPayload {
+         address: init.contract_address,
+         amount: Amount::zero(),
+         receive_name: OwnedReceiveName::new_unchecked("gonana_marketplace.confirm_order".to_string()),
+         message: OwnedParameter::from_serial(&PlaceOrderParameter {
+             product_name: "Oranges".to_string(),
+             buyer: BOB,
+             seller: ALICE,
+         })
+         .expect("Parameter within size bounds"),
+     },
+ );
+        assert!(update.is_ok());
+        assert_eq!(chain.account_balance(ALICE).unwrap().total, (alice_balance_before.total + purchase_amount));
+
+
+//     // Verify that the product is in the Confirmed state.
+    let product_listings_after_confirm: Vec<ProductListing> =
+        chain
+            .contract_update(
+                SIGNER,
+                ALICE,
+                Address::Account(ALICE),
+                Energy::from(10000),
+                UpdateContractPayload {
+                    amount: Amount::zero(),
+                    address: init.contract_address,
+                    receive_name: OwnedReceiveName::new_unchecked("gonana_marketplace.view_product_listings".to_string()),
+                    message: OwnedParameter::empty(),
+                },
+            )
+            .expect("View product listings after confirm succeeds.")
+            .parse_return_value()
+            .expect("Deserialize product listings");
+
+    assert_eq!(product_listings_after_confirm.len(), 1);
+    assert_eq!(product_listings_after_confirm[0].state, ProductState::Confirmed);
 }
-
-
-
-
-
