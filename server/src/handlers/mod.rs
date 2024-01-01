984f8fb9438a41fdb2d0b4bf9082e8ab45f9826d
Ebuka <ebuka2264@yahoo.com>
2024-01-01 11:42:11 +0100

    feat: server initialized

---
+++ server/src/handlers/mod.rs
@@ -0,0 +1,5 @@
+mod api;
+mod types;
+pub fn help(){
+    
+}
---
+++ server/src/main.rs
@@ -0,0 +1,11 @@
+mod handlers;
+
+use actix_web::{get,post,patch};
+
+use crate::handlers::help;
+
+
+fn main() {
+    println!("Hello, world!");
+    
+}
